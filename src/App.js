11b0f6b3d356b4c12b21ec521540d2ab02c68ead
Rafe Uddaraj <78890044+rafeuddaraj@users.noreply.github.com>
2021-09-23 20:02:36 +0600

    React File Setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import Clock from './components/Clock';
+// import MyFrom from './components/Form';
+function App() {
+  // const quantity = [1, 2, 3];
+  return (
+    <div>
+      Hello I am Rafe Uddaraj
+    </div>
+  );
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import reportWebVitals from './reportWebVitals'
+import App from './App'
+
+reactDom.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
